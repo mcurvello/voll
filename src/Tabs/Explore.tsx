b301c91f3b19c1b557e55f07a94d608229f0a74e
Marcio Curvello <marciocurvello@gmail.com>
2023-10-08 11:26:15 -0300

    add main and explore screens

--- src/Tabs/Explore.tsx
+++ src/Tabs/Explore.tsx
@@ -1,9 +1,52 @@
-import { Text, VStack } from "native-base";
+import { Box, ScrollView, Text, VStack } from "native-base";
+import { InputText } from "../components/InputText";
+import { MainButton } from "../components/MainButton";
+import { Title } from "../components/Title";
+import { AppointmentCard } from "../components/AppointmentCard";
 
 export default function Explore() {
   return (
-    <VStack>
-      <Text>Explorar</Text>
-    </VStack>
+    <ScrollView flex={1} bgColor="white">
+      <VStack
+        flex={1}
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        p={5}
+      >
+        <Box
+          w="100%"
+          borderRadius="lg"
+          p={3}
+          mt={5}
+          shadow="1"
+          borderRightRadius="md"
+          bgColor="white"
+        >
+          <InputText placeholder="Digite a especialidade" />
+          <InputText placeholder="Digite sua localização" />
+          <MainButton mt={3} mb={3}>
+            Buscar
+          </MainButton>
+        </Box>
+        <Title color="blue.500" alignSelf="center" mb={5}>
+          Resultado da Busca
+        </Title>
+        {[1, 2, 3].map((_, index) => (
+          <VStack
+            flex={1}
+            w="100%"
+            alignItems="flex-start"
+            bgColor="white"
+            key={index}
+          >
+            <AppointmentCard
+              area="Cardiologia"
+              name="Dr. Marcio"
+              photo="https://github.com/mcurvello.png"
+            />
+          </VStack>
+        ))}
+      </VStack>
+    </ScrollView>
   );
 }
--- src/Tabs/Main.tsx
+++ src/Tabs/Main.tsx
@@ -1,9 +1,58 @@
-import { Text, VStack } from "native-base";
+import { Box, Divider, Image, ScrollView, Text, VStack } from "native-base";
+import Logo from "../assets/Logo.png";
+import { Title } from "../components/Title";
+import { InputText } from "../components/InputText";
+import { MainButton } from "../components/MainButton";
+import { depoimentos } from "../utils/mock";
 
 export default function Main() {
   return (
-    <VStack>
-      <Text>Principal</Text>
-    </VStack>
+    <ScrollView flex={1} bgColor="white">
+      <VStack
+        flex={1}
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        p={5}
+      >
+        <Image source={Logo} alt="Logo" mt={10} />
+        <Title color="blue.500">Boas-vindas!</Title>
+        <Box
+          w="100%"
+          borderRadius="lg"
+          p={3}
+          mt={10}
+          shadow="1"
+          borderRightRadius="md"
+          bgColor="white"
+        >
+          <InputText placeholder="Digite a especialidade" />
+          <InputText placeholder="Digite sua localização" />
+          <MainButton mt={3} mb={3}>
+            Buscar
+          </MainButton>
+        </Box>
+        <Title color="blue.800" alignSelf="center">
+          Depoimentos
+        </Title>
+        <VStack space={3} divider={<Divider />} w="100%">
+          {depoimentos.map((depoimento) => (
+            <Box key={depoimento.id} w="100%" borderRadius="lg" p={3}>
+              <Text color="gray.300" fontSize="md" textAlign="justify">
+                {depoimento.text}
+              </Text>
+              <Text
+                color="gray.500"
+                fontSize="lg"
+                fontWeight="bold"
+                alignSelf="center"
+                mt={2}
+              >
+                {depoimento.titulo}
+              </Text>
+            </Box>
+          ))}
+        </VStack>
+      </VStack>
+    </ScrollView>
   );
 }
