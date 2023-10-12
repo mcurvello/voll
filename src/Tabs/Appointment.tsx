3a80b25bf63a007ea28e4619b3fb661f2e201462
Marcio Curvello <marciocurvello@gmail.com>
2023-10-12 14:30:54 -0300

    update appointmens screen

--- src/Tabs/Appointment.tsx
+++ src/Tabs/Appointment.tsx
@@ -2,8 +2,52 @@ import { Divider, ScrollView } from "native-base";
 import { AppointmentCard } from "../components/AppointmentCard";
 import { Title } from "../components/Title";
 import { MainButton } from "../components/MainButton";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getAppointmentsPatient } from "../services/patientService";
+
+interface IEspecialista {
+  id: string;
+  nome: string;
+  imagem: string;
+  especialidade: string;
+}
+
+interface IConsulta {
+  id: string;
+  data: string;
+  especialista: IEspecialista;
+}
 
 export default function Appointment() {
+  const [proximasConsultas, setProximasConsultas] = useState<IConsulta[]>([]);
+  const [consultasPassadas, setConsultasPassadas] = useState<IConsulta[]>([]);
+
+  useEffect(() => {
+    async function getAppointments() {
+      const pacienteId = await AsyncStorage.getItem("pacienteId");
+      if (!pacienteId) return null;
+
+      const todasConsultas: IConsulta[] = await getAppointmentsPatient(
+        pacienteId
+      );
+
+      const now = new Date();
+
+      const proximas = todasConsultas.filter(
+        (consulta) => new Date(consulta.data) > now
+      );
+
+      const passadas = todasConsultas.filter(
+        (consulta) => new Date(consulta.data) <= now
+      );
+
+      setProximasConsultas(proximas);
+      setConsultasPassadas(passadas);
+    }
+    getAppointments();
+  }, []);
+
   return (
     <ScrollView p={5} mt={5}>
       <Title color="blue.500">Minhas consultas</Title>
@@ -13,40 +57,32 @@ export default function Appointment() {
       <Title color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>
         Próximas consultas
       </Title>
-      <AppointmentCard
-        name="Dr. Marcio"
-        area="Cardiologista"
-        photo="https://github.com/mcurvello.png"
-        date="20/04/2023"
-        wasScheduled
-      />
+      {proximasConsultas?.map((consulta) => (
+        <AppointmentCard
+          key={consulta?.id}
+          name={consulta?.especialista?.nome}
+          area={consulta?.especialista?.especialidade}
+          photo={consulta?.especialista?.imagem}
+          date={consulta?.data}
+          wasScheduled
+        />
+      ))}
 
       <Divider mt={2} />
 
       <Title color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>
         Consultas passadas
       </Title>
-      <AppointmentCard
-        name="Dr. Marcio"
-        area="Cardiologista"
-        photo="https://github.com/mcurvello.png"
-        date="20/04/2023"
-        wasAttended
-      />
-      <AppointmentCard
-        name="Dr. Marcio"
-        area="Cardiologista"
-        photo="https://github.com/mcurvello.png"
-        date="20/04/2023"
-        wasAttended
-      />
-      <AppointmentCard
-        name="Dr. Marcio"
-        area="Cardiologista"
-        photo="https://github.com/mcurvello.png"
-        date="20/04/2023"
-        wasAttended
-      />
+      {consultasPassadas?.map((consulta) => (
+        <AppointmentCard
+          key={consulta?.id}
+          name={consulta?.especialista?.nome}
+          area={consulta?.especialista?.especialidade}
+          photo={consulta?.especialista?.imagem}
+          date={consulta?.data}
+          wasAttended
+        />
+      ))}
     </ScrollView>
   );
 }
--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -23,3 +23,13 @@ export async function getPatient(id: string) {
     return null;
   }
 }
+
+export async function getAppointmentsPatient(id: string) {
+  try {
+    const result = await api.get(`/paciente/${id}/consultas`);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
